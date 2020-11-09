060bb05f98549f963053d22ca723c16c15acf3ad
Francis Langit <francis.villanueva.langit@gmail.com>
2020-11-09 15:42:24 +0800

    Create spritesheet.py

--- main.py
+++ main.py
@@ -1,41 +1,58 @@
 import pygame, sys
 
+from settings import Settings
+
 
 class Bolan:
-	"""Overall class to manage game assets and behavior."""
+	"""
+	Overall class to manage game assets and behavior.
+	"""
 
 
 	def __init__(self):
-		"""Initialize the game."""
+		"""
+		Initialize the game.
+		"""
 		pygame.init()
+		self.settings = Settings()
 
-		self.screen = pygame.display.set_mode((1280, 600))
+		self.screen = pygame.display.set_mode((
+			self.settings.screen_width,
+			self.settings.screen_height,
+		))
 		pygame.display.set_caption("Bolan")
 
 
 	def run(self):
-		"""Start the game's main loop."""
+		"""
+		Start the game's main loop.
+		"""
 		while True:
 			self._check_events()
 			self._update_screen()
 
 
 	def _check_events(self):
-		"""Track events and user input."""
+		"""
+		Track events and user input.
+		"""
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
 
 			elif event.type == pygame.KEYDOWN:
 				if event.key in (pygame.K_q, pygame.K_ESCAPE):
-					sys.exit()		
+					sys.exit()
 
 
 	def _update_screen(self):
-		"""Update display of game."""
+		"""
+		Update display of game.
+		"""
 		self.screen.fill((255, 255, 255))
 		pygame.display.flip()
 
 
-bolan = Bolan()
-bolan.run()
+if __name__ == '__main__':
+	bolan = Bolan()
+	bolan.run()
