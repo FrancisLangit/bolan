0ca9762ade5a278770c36ae9406312b030192ab9
Francis Langit <francis.villanueva.langit@gmail.com>
2020-11-09 15:09:21 +0800

    Create settings.py
    Screen width, screen height, and background color settings were
    placed into the file during this commit.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+	"""Hold all settings of game."""
+
+
+	def __init__(self):
+		"""Initialize Settings class."""
+		self.screen_width = 1280
+		self.screen_height = 600
+		self.background_color = (255, 255, 255)
