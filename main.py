b52c957cd7662e56d787cdbbb22dd71f14349b48
Francis Langit <francis.villanueva.langit@gmail.com>
2020-11-09 15:05:31 +0800

    Refactor main.py

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 import pygame, sys
-pygame.init()
 
-screen = pygame.display.set_mode((1280, 600)) 
-pygame.display.set_caption("Bolan.py")
 
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+class Bolan:
+	"""Overall class to manage game assets and behavior."""
 
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_q:
+
+	def __init__(self):
+		"""Initialize the game."""
+		pygame.init()
+
+		self.screen = pygame.display.set_mode((1280, 600))
+		pygame.display.set_caption("Bolan")
+
+
+	def run(self):
+		"""Start the game's main loop."""
+		while True:
+			self._check_events()
+			self._update_screen()
+
+
+	def _check_events(self):
+		"""Track events and user input."""
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
 				sys.exit()
+
+			elif event.type == pygame.KEYDOWN:
+				if event.key in (pygame.K_q, pygame.K_ESCAPE):
+					sys.exit()		
+
+
+	def _update_screen(self):
+		"""Update display of game."""
+		self.screen.fill((255, 255, 255))
+		pygame.display.flip()
+
+
+bolan = Bolan()
+bolan.run()
