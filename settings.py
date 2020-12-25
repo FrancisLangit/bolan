9c2bd4baaaf626fc5afb91ed06260bffb1aa77d8
francis <francis.villanueva.langit@gmail.com>
2020-12-25 18:39:40 +0800

    Bring down max_fps to 600

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ class Settings:
 		self.background_color = (255, 255, 255)
 		self.display_caption = "Bolan.py"
 		self.spritesheet_colorkey = (0, 0, 0)
-		self.max_fps = 750
+		self.max_fps = 600
 
 
 		# Title settings 
