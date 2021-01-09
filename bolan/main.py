1a03d982013465b9a9e7e40e65b85a7d881d1883
francis <francis.villanueva.langit@gmail.com>
2021-01-09 18:08:39 +0800

    Move main module files to separate repository

---
+++ bolan/main.py
@@ -0,0 +1,196 @@
+import pygame, sys
+
+import objects
+
+from settings import Settings
+from spritesheet import SpriteSheet
+
+
+class BolanGame:
+	""" 
+	Overall class to manage game assets and behavior.
+	"""
+
+
+	def __init__(self):
+		"""
+		Initialize the game
+		"""
+		pygame.init()
+		self.clock = pygame.time.Clock()
+		self.spritesheet = SpriteSheet('assets/spritesheet.png')
+		self.settings = Settings(self)
+
+		self.screen = pygame.display.set_mode((
+			self.settings.screen_width,
+			self.settings.screen_height,
+		))
+		self.screen_rect = self.screen.get_rect()
+		pygame.display.set_caption(self.settings.display_caption)
+
+		# Track gamestate.
+		self.is_play = False
+		self.is_gameover = False
+
+		# Game objects.
+		self.title = objects.Title(self)
+		self.gameover_images = objects.GameOverImages(self)
+		self.scoreboard = objects.Scoreboard(self)
+		self.floor = objects.Floor(self)
+		self.clouds = objects.Clouds(self)
+		self.bolan = objects.Bolan(self)
+		self.obstacles = objects.Obstacles(self)
+
+
+	def run(self):
+		"""
+		Start the game's main loop.
+		"""
+		while True:
+			self.clock.tick(self.settings.max_fps)
+			self._check_events()
+			self._update_screen()
+
+
+	def _check_events(self):
+		"""	
+		Track events and user input.
+		"""	
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+			elif event.type == pygame.KEYDOWN:
+				self._check_keydown_events(event)
+
+			elif event.type == pygame.KEYUP:
+				self._check_keyup_events(event)
+
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				self._check_retry_button(pygame.mouse.get_pos())
+
+
+	def _check_keydown_events(self, event):
+		"""
+		Checks keydown events.
+		"""
+		if event.key in (pygame.K_q, pygame.K_ESCAPE):
+			self._exit_program()
+		elif self.is_play:
+			self._check_keydown_play_events(event)
+		elif self.is_gameover:
+			if event.key in (pygame.K_RETURN, pygame.K_r):
+				self._reset_game()
+		else:
+			if event.key == pygame.K_RETURN:
+				self.is_play = True
+
+
+	def _check_keydown_play_events(self, event):
+		"""
+		Keydown events when is_play is True.
+		"""
+		if (event.key in (pygame.K_SPACE, pygame.K_UP)) and (
+			self.bolan.rect.y >= self.bolan.default_y):
+			self.bolan.is_jump = True
+		if event.key == pygame.K_DOWN:
+			self.bolan.is_duck = True
+
+
+	def _check_keyup_events(self, event):
+		"""
+		Checks keyup events.
+		"""
+		if event.key == pygame.K_DOWN:
+			self.bolan.is_duck = False
+
+
+	def _check_retry_button(self, mouse_position):
+		"""
+		Checks if the user clicks the mousebutton within the retry button.
+		"""
+		if self.gameover_images.retry_rect.collidepoint(mouse_position):
+			self._reset_game()
+			
+
+	def _reset_game(self):
+		"""
+		Resets the game to its settings upon startup.
+		"""
+		self.is_play = True
+		self.is_gameover = False
+		self.bolan.rect.y = self.bolan.default_y
+		self.scoreboard.score = 0
+		self.obstacles._reset_positions()
+
+
+	def _exit_program(self):
+		"""
+		Saves the highscore and exits the program.
+		"""
+		with open("assets/highscore.txt", 'w') as highscore:
+			highscore.write(str(self.scoreboard.highscore))
+		sys.exit()
+
+
+	def _update_screen(self):
+		"""
+		Update display of game.
+		"""
+		self.screen.fill((255, 255, 255))
+
+		self._check_collisions()
+		self._update_objects()
+		self._blit_objects()
+
+		pygame.display.flip()
+
+
+	def _check_collisions(self):
+		"""
+		Respond to collisions between Bolan and an obstacles.
+		"""
+		if pygame.sprite.spritecollide(
+			self.bolan, 
+			self.obstacles.obstacles,
+			False, 
+			pygame.sprite.collide_mask):
+				self.is_play = False
+				self.is_gameover = True
+				self.bolan.image = self.settings.bolan_dead_image
+
+
+	def _update_objects(self):
+		"""
+		Updates the game objects.
+		"""
+		self.clouds.update()
+		if self.is_play:
+			self.floor.update()
+			self.bolan.update()
+			self.obstacles.update()
+			self.scoreboard.update()
+
+
+	def _blit_objects(self):
+		"""
+		Blits the game objects onto the screen.
+		"""
+		self.clouds.blitme()
+		self.floor.blitme()
+		self.bolan.blitme()
+		self.obstacles.blitme()
+		self._blit_text()
+		self.scoreboard.blitme()
+
+
+	def _blit_text(self):
+		if not self.is_play and not self.is_gameover:
+			self.title.blitme()
+		elif self.is_gameover:
+			self.gameover_images.blitme()
+
+
+if __name__ == '__main__':
+	bolan_game = BolanGame()
+	bolan_game.run()
---
+++ bolan/settings.py
@@ -0,0 +1,111 @@
+import pygame
+
+from spritesheet import SpriteSheet
+
+
+class Settings:
+	"""
+	Holds all settings of game.
+	"""
+
+
+	def __init__(self, bolan_game):
+		"""
+		Initialize Settings class.
+		"""
+		self.bolan_game = bolan_game
+		self.spritesheet = bolan_game.spritesheet
+
+		# General settings
+		self.screen_width = 1280
+		self.screen_height = 600
+		self.background_color = (255, 255, 255)
+		self.display_caption = "Bolan.py"
+		self.spritesheet_colorkey = (0, 0, 0)
+		self.max_fps = 600
+		self.score_increment_rate = 80 # Rate at which score increments.
+
+
+		# Title settings 
+		self.title_font = pygame.font.Font(
+			"assets/fonts/PressStart2P.ttf", 32)
+		self.title_image = self.title_font.render(
+			"B O L A N . P Y", True, (83, 83, 83))
+		self.subtitle_font = pygame.font.Font(
+			"assets/fonts/Fipps.otf", 16)
+		self.subtitle_image = self.subtitle_font.render(
+			"Press ENTER to Play", True, (83, 83, 83))
+
+
+		# GameOverImages settings
+		self.gameover_image = self.spritesheet.image_at(
+			(1294, 29, 381, 21), self.spritesheet_colorkey)
+		self.retry_image = self.spritesheet.image_at(
+			(2, 2, 72, 64), self.spritesheet_colorkey)
+
+
+		# Bolan settings
+		self.bolan_x_position = 20
+		self.bolan_y_position = 440
+
+		self.bolan_run_images = self.spritesheet.load_strip(
+			(1854, 2, 88, 94), 2, self.spritesheet_colorkey)
+		self.bolan_duck_images = self.spritesheet.load_strip(
+			(2206, 6, 118, 94), 2, self.spritesheet_colorkey)
+
+		self.bolan_standing_image = self.spritesheet.image_at(
+			(1678, 2, 88, 94), self.spritesheet_colorkey)
+		self.bolan_dead_image = self.spritesheet.image_at(
+			(2030, 2, 88, 94), self.spritesheet_colorkey)
+		
+		self.bolan_update_rate = 60 # Update Bolan's image every 60 ticks.
+
+
+		# Pterodactyl settings
+		self.pterodactyl_images = self.spritesheet.load_strip(
+			(261, 2, 91, 80), 2, self.spritesheet_colorkey)
+
+		# Update Pterodactyl's image every 150 ticks.
+		self.pterodactyl_update_rate = 150
+
+		# Heights at which the Pterodactyl can spawn.
+		# 520: Jump | 460: Jump/Duck | 410: Run Under |
+		self.pterodactyl_heights = [520, 460, 410]
+
+		# Score at which Pterodactyls can start spawning.
+		self.pterodactyl_min_score = 350 
+		
+
+		# Floor settings
+		self.floor_rect = [2, 104, 2400, 26]
+		self.floor_y = 500
+		self.floor_speed = 1
+		self.floor_image = self.spritesheet.image_at(
+			self.floor_rect, self.spritesheet_colorkey)
+
+
+		# Cactus settings
+		self.cactus_small_images = self.spritesheet.load_strip(
+			(447, 28, 34, 70), 6, self.spritesheet_colorkey)
+		self.cactus_big_images = self.spritesheet.load_strip(
+			(652, 2, 50, 100, 4), 4, self.spritesheet_colorkey)
+		self.cactus_group_images = self.spritesheet.images_at([
+			(481, 28, 68, 70), # Two small cacti.
+			(549, 28, 102, 70), # Three small cacti.
+			(802, 2, 150, 100), # Three big cacti, one small cactus.
+			(702, 2, 100, 100), ], # Two big cacti.
+			self.spritesheet_colorkey,
+		)
+		self.cactus_images = (
+			self.cactus_small_images +
+			self.cactus_big_images + 
+			self.cactus_group_images 
+		)
+
+
+		# Cloud settings
+		self.cloud_image = self.spritesheet.image_at(
+			(166, 2, 92, 27), self.spritesheet_colorkey)
+		self.cloud_x_range = range(100, 1180)
+		self.cloud_y_range = range(75, 300)
+		self.cloud_number = 6
